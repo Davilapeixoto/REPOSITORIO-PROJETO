7308903e11b3d57f7ce397e665b58616013919a1
Davilapeixoto <davilapeixoto52@gmail.com>
2025-03-25 19:13:28 -0300

    alguns reajustes falados na monitoria

---
+++ projeto/solidare/contas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+#class func_usuarios(AbstractUser):
+#    USUARIOS=(("aluno","Aluno"),("professor","Professor"),("admin","Administrador"), ("patrocinador","Patrocinador"))
+#    tipo_usuario=models.CharField(max_length=12,choices=USUARIOS, default="aluno")
+
+    
