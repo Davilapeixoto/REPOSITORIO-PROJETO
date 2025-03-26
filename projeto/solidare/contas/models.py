89fcdc3f7c63eb5d19f890d34ef75501fb646ea3
Davilapeixoto <davilapeixoto52@gmail.com>
2025-03-26 05:04:43 -0300

    cadastro

--- projeto/solidare/contas/models.py
+++ projeto/solidare/contas/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-#class func_usuarios(AbstractUser):
-#    USUARIOS=(("aluno","Aluno"),("professor","Professor"),("admin","Administrador"), ("patrocinador","Patrocinador"))
-#    tipo_usuario=models.CharField(max_length=12,choices=USUARIOS, default="aluno")
+class func_usuarios(AbstractUser):
+    USUARIOS=(("aluno","Aluno"),
+              ("professor","Professor"),
+              ("admin","Administrador"), 
+              ("patrocinador","Patrocinador"))
+    tipo_usuario=models.CharField(max_length=12,choices=USUARIOS, default="aluno")
 
     
--- projeto/solidare/contas/views.py
+++ projeto/solidare/contas/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.hashers import make_password
+from .models import func_usuarios
 
 # Create your views here.
+
+def cadastro(request):
+    if request.method =="POST":
+        id=request.POST.get("id")
+        email=request.POST.get("email")
+        senha=request.POST.get("senha")
+        tipo_usuario=request.POST.get("tipo_usuario")
+        nome=request.POST.get("nome")
+        sobrenome=request.POST.get("sobrenome")
+        if id and email and senha and tipo_usuario and nome and sobrenome:
+            user= func_usuarios(id=id,email=email,tipo_usuario=tipo_usuario,nome=nome,sobrenome=sobrenome)
+            user.save()
+            login(request,user)
+            return redirect("login")
+        
+    return render(request,'contas/cadastro.html')
